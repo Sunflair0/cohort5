4688d281ab020c9957a6a4c304a038892a24b668
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-06-28 16:00:09 -0500

    toggle done

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 function blink() {
-let  star = document.getElementById("star");
-star.classList.toggle("star_blink");
+	let star = document.getElementById("star");
+	star.classList.toggle("star_blink");
+}
+
+let vc= document.querySelector('.video_container');
+let vid = document.querySelector('.vid');
+
+let play = false;
+vid.addEventListener('click', playVideo);
+vc.addEventListener('click', playVideo);
+
+
+function playVideo() {
+
+
+	if (!play) {
+		document.querySelector(".video").style.cssText = " transform: translate(-50%,-50%)";
+
+		play = true;
+	} else {
+
+		document.querySelector(".video").style.cssText = " transform: translate(-50%,-200%)";
+
+		play = false;
+	}
 }
