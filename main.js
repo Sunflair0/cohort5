ba26bbbacf95ac4b4c82a5121819be5cb2cac855
Sunflair0 <62845098+Sunflair0@users.noreply.github.com>
2023-07-07 17:24:47 -0500

    target removed, section spacing done

--- main.js
+++ main.js
@@ -34,66 +34,12 @@ function playVideo() {
 }
 
 
-// let site = document.getElementsByClassName("site");
-
-let x = "one";
-
-let idArray = ["one", "two", "three", "four", "five", "six", "seven"];
-
-// document.getElementById(`one`).addEventListener("mouseover", degreeProgression);
-// document.getElementById(`one`).addEventListener("mouseout", degreeStop);
-
-
-
-
-function degreeProgression() {
-
-	let counter = 0;
-	let reset = 50;
-
-	const resetRun = setInterval(() => {
-		{
-			if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') == 1) {
-
-				if (counter == reset) {
-					counter = 0;
-
-				}
-				counter++
-				console.log(counter)
-				document.getElementById(`one`).style.backgroundImage = `radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%), 
-									repeating-radial-gradient(
-									transparent ${counter - 35}%, 
-									black ${counter - 45}%, 
-									transparent ${counter - 50}%,
-									black ${counter - 25}%, 
-									transparent ${counter - 10}%),
-									radial-gradient(#ffffff4a 0%, rgba(255, 255, 255, 0.2) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)`;
-
-
-			} else if (getComputedStyle(document.getElementById(`one`)).getPropertyValue('--hovered') !== 1) {
-				clearInterval(resetRun);
-				degreeStop();
-			}
-		}
-
-	}, 50);
-}
-
-function degreeStop() {
-	console.log("done");
-	document.getElementById(`one`).style.backgroundImage = 'radial-gradient(rgb(255, 255, 255) 0%, rgba(255, 255, 255, 0.75) 20%, transparent 55%, rgba(0, 0, 0, 0.5) 65%)';
-
-}
-
 const bubble = document.querySelectorAll(".bubble");
 const mbubble = document.querySelectorAll(".mbubble");
 const duck = document.querySelector("#duck");
-console.log(bubble);
 document.getElementById('duck').addEventListener("mouseover", addBubbles);
 
 function addBubbles() {
-	console.log("in");
 	bubble.forEach((elem) => {
 		console.log(elem);
 		elem.classList.add('bubbles');
@@ -108,16 +54,12 @@ function addBubbles() {
 	}
 }
 
-
 function delayBubbleFade() {
-	const bubbleStream = setTimeout(removeBubbles, 2550);
-
+	const bubbleStream = setTimeout(removeBubbles, 2400);
 	return bubbleStream;
 }
 
-
 function removeBubbles(bubbleStream) {
-	console.log("out");
 	bubble.forEach((elem) => {
 		elem.classList.remove('bubbles');
 	});
@@ -125,11 +67,9 @@ function removeBubbles(bubbleStream) {
 		elem.classList.remove('bubbles2');
 	});
 
-
 	if (duck) {
 		duck.addEventListener("mouseover", addBubbles);
 		duck.removeEventListener("mouseout", delayBubbleFade);
 		clearTimeout(bubbleStream);
 	}
-
 }
